dda8d58d184912af9086698c1de4a22c6a79de69
jelenatabak <jelena.tabak@gmail.com>
2022-05-04 09:48:30 +0200

    calculate and store indices

--- scripts/agrosparc.py
+++ scripts/agrosparc.py
@@ -34,6 +34,7 @@ import urllib.request
 import pysftp
 import shutil
 
+
 class CameraModel:
     def __init__(self, path):
         self.path = path
@@ -88,10 +89,14 @@ class DataCollector:
 
         self.ms_camera = []
         for i in range(1, 6):
+            # xml_path = '/home/jelena/catkin_ws_old/src/micasense_camera/intrinsics/ms_red_band_' + \
+            #     str(i) + '.xml'
             xml_path = '/home/franka/catkin_ws/src/micasense_camera/intrinsics/ms_red_band_' + \
                 str(i) + '.xml'
             self.ms_camera.append(CameraModel(xml_path))
         for i in range(1, 6):
+            # xml_path = '/home/jelena/catkin_ws_old/src/micasense_camera/intrinsics/ms_blue_band_' + \
+            #     str(i) + '.xml'
             xml_path = '/home/franka/catkin_ws/src/micasense_camera/intrinsics/ms_blue_band_' + \
                 str(i) + '.xml'
             self.ms_camera.append(CameraModel(xml_path))
@@ -144,8 +149,12 @@ class DataCollector:
         remote_host = '161.53.68.231'
         remote_user = 'agrosparc_user'
         remote_user_pass = 'vege123!'
-        self.srv = pysftp.Connection(host=remote_host, username=remote_user, password=remote_user_pass)
+        self.srv = pysftp.Connection(
+            host=remote_host, username=remote_user, password=remote_user_pass)
 
+        # [min_y, min_x, max_y, max_x]
+        self.lower_bbox = [280, 320, 470, 380]
+        self.upper_bbox = [80, 320, 270, 380]
 
     def collect_data(self):
         while not rospy.is_shutdown():
@@ -158,38 +167,35 @@ class DataCollector:
 
                 print('Collecting data')
                 self.ts = datetime.datetime.now()
-                self.pc_msg = rospy.wait_for_message(self.pc_topic, PointCloud2)
+                self.pc_msg = rospy.wait_for_message(
+                    self.pc_topic, PointCloud2)
                 self.rs_msg = rospy.wait_for_message(self.rs_topic, Im)
                 print('Recieved pc and rs messages')
                 self.capture()
                 self.store_data()
 
-                # self.generate_mspc()
-                # self.calculate_indices()
-                self.publish_data()
-                print('Data written to DB')
-
                 ts = self.cam['timestamp'].iloc[-1]
-                update_camera_trigger(False,'laptop',ts,self.conn)
+                update_camera_trigger(False, 'laptop', ts, self.conn)
                 print('Trigger updated')
+
+                self.generate_mspc()
+                self.calculate_indices(self.lower_bbox, self.plant_id_lower)
+                self.calculate_indices(self.upper_bbox, self.plant_id_upper)
+
+                # copy to server and delete local copy
+                self.srv.put_r(self.local_path, self.path, preserve_mtime=True)
+                shutil.rmtree(self.local_path)
+
+                print('Data written to DB')
 
             else:
                 print("Waiting for the trigger...")
                 rospy.sleep(3)
 
-    def publish_data(self):
-        batch_ts = self.ts
-        ts = self.ts
-        image_path = self.path
-        insert_values = (batch_ts, ts, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, self.plant_id_lower, self.zone_id, self.device_id, image_path)
-        insert_camera_measurements(insert_values, self.conn)
-        insert_values = (batch_ts, ts, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, self.plant_id_upper, self.zone_id, self.device_id, image_path)
-        insert_camera_measurements(insert_values, self.conn)
-
-
     def store_data(self):
         os.mkdir(self.local_path)
-        self.path = os.path.join(self.base_path, str(self.device_id) + "_" + str(self.zone_id) + "_" + str(self.ts))
+        self.path = os.path.join(self.base_path, str(
+            self.device_id) + "_" + str(self.zone_id) + "_" + str(self.ts))
         self.srv.makedirs(self.path, mode=777)
 
         # save bag files
@@ -203,10 +209,12 @@ class DataCollector:
 
         # save ms_red images
         paths = self.r.json().get('raw_storage_path')
-        for i in range(1,6):
+        self.images = []
+        for i in range(1, 6):
             path = paths.get(str(i))
             path_split = path.split('/')
-            write_path = os.path.join(self.local_path, path_split[2], path_split[3])
+            write_path = os.path.join(
+                self.local_path, path_split[2], path_split[3])
             print(write_path)
             os.umask(000)
             os.makedirs(write_path, mode=0o777, exist_ok=True)
@@ -215,69 +223,24 @@ class DataCollector:
             f.write(urllib.request.urlopen(req).read())
             f.close()
             print('Image saved: ' + write_path)
+            self.images.append(Image(write_path).undistorted_reflectance())
 
-        # copy to server and delete local copy
-        self.srv.put_r(self.local_path, self.path, preserve_mtime=True)
-        shutil.rmtree(self.local_path)
-
-    def calculate_indices(self):
-        self.ndvi_array = np.copy(self.pc_array)
-        for i in range(self.pc_height):
-            for j in range(self.pc_width):
-                nir = self.ms_array[i, j, 3]
-                red = self.ms_array[i, j, 2]
-
-                if not isnan(nir) and not isnan(red):
-                    self.ndvi_array[i, j][3] = (nir-red)/(nir+red)
-                else:
-                    self.ndvi_array[i, j][3] = NaN
-    
-    def calculate_indices_test(self):
-        pc_pub = rospy.Publisher("/test_pc", PointCloud2, queue_size=10)
-
-        self.images = []
-        for i in range(1, 6):
-            path = '/home/jelena/bags/agrosparc_test/IMG_0003_' + str(i) + '.tif'
-            #self.images.append(cv2.imread(path, cv2.IMREAD_UNCHANGED))
-            self.images.append(Image(path).undistorted_reflectance())
-
-        self.ms_bands = 5
-
-        test_bag = rosbag.Bag('/home/jelena/bags/agrosparc_test/test.bag')
-        for topic, msg, t in test_bag.read_messages(topics=['camera/depth_registered/points']):
-            self.pc_array = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
-
-        self.array = np.copy(self.pc_array)
-        self.pc_height = self.pc_array.shape[0]
-        self.pc_width = self.pc_array.shape[1]
-        self.ms_array = np.full(
-            [self.pc_height, self.pc_width, self.ms_bands], NaN)
-
-        for i in range(self.pc_height):
-            for j in range(self.pc_width):
-                pt = self.array[i][j]
-
-                if not isnan(pt[0]):
-                    point = np.matrix([[pt[0]], [pt[1]], [pt[2]], [1]])
-
-                    for n in range(self.ms_bands):
-                        point_transformed = np.dot(self.T[n], point)
-
-                        u, v = self.ms_camera[n].get_pixel_coordinates(
-                            point_transformed)
-                        if u is not None and v is not None:
-                            self.ms_array[i, j, n] = self.images[n][v, u]
-
-        self.ndvi_array = np.copy(self.pc_array)
-        nir_thresh = 0.15
+    def calculate_indices(self, bbox, plant_id):
         ind = np.zeros(12)
         counter = 0
 
-        # for i in range(self.pc_height):
-        #     for j in range(self.pc_width):
+        nir_total = 0
+        nir_counter = 0
 
-        for i in range(320,480):
-            for j in range(300,390):
+        for i in range(bbox[0], bbox[2]):
+            for j in range(bbox[1], bbox[3]):
+                nir_total += self.ms_array[i, j, 3]
+                nir_counter += 1
+
+        nir_thresh = nir_total/nir_counter
+
+        for i in range(bbox[0], bbox[2]):
+            for j in range(bbox[1], bbox[3]):
                 blue = self.ms_array[i, j, 0]
                 green = self.ms_array[i, j, 1]
                 red = self.ms_array[i, j, 2]
@@ -290,8 +253,95 @@ class DataCollector:
                     ind[1] += nir/red
                     ind[2] += (nir-green)/(nir+green)
                     ind[3] += 0.16*(nir-red)/(nir+red+0.16)
-                    ind[4] += (2*nir + 1 - sqrt(pow((2*nir+1), 2) - 8*(nir-red))) / 2
-                    ind[5] += 1.5*(1.2*(nir-green)-2.5*(red-green)) / sqrt(pow((3*nir+1),2)-(6*nir-5*sqrt(red))-0.5) 
+                    ind[4] += (2*nir + 1 -
+                               sqrt(pow((2*nir+1), 2) - 8*(nir-red))) / 2
+                    ind[5] += 1.5*(1.2*(nir-green)-2.5*(red-green)) / \
+                        sqrt(pow((3*nir+1), 2)-(6*nir-5*sqrt(red))-0.5)
+                    ind[6] += 1.16*(nir-red)/(nir+red+0.16)
+                    ind[7] += nir - re/nir + re
+                    ind[8] += 2.5*(nir-red) / (nir+6*red-7.5*blue+1)
+                    ind[9] += 0.5*(120*(nir-green)-200*(red-green))
+                    ind[10] += (nir-red) / sqrt(nir+red)
+                    ind[11] += nir-red
+
+                    self.ndvi_array[i, j][3] = (nir-red)/(nir+red)
+                else:
+                    self.ndvi_array[i, j][3] = NaN
+
+        avg_indices = ind / counter
+        insert_values = (self.ts, self.ts, *avg_indices, plant_id, self.zone_id, self.device_id, self.path)
+        insert_camera_measurements(insert_values, self.conn)
+
+
+    def calculate_indices_test(self):
+        #pc_pub = rospy.Publisher("/test_pc", PointCloud2, queue_size=10)
+
+        # self.images = []
+        # for i in range(1, 6):
+        #     path = '/home/jelena/bags/agrosparc_test/IMG_0003_' + str(i) + '.tif'
+        #     #self.images.append(cv2.imread(path, cv2.IMREAD_UNCHANGED))
+        #     self.images.append(Image(path).undistorted_reflectance())
+
+        # self.ms_bands = 5
+
+        # test_bag = rosbag.Bag('/home/jelena/bags/agrosparc_test/test.bag')
+        # for topic, msg, t in test_bag.read_messages(topics=['camera/depth_registered/points']):
+        #     self.pc_array = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
+
+        # self.array = np.copy(self.pc_array)
+        # self.pc_height = self.pc_array.shape[0]
+        # self.pc_width = self.pc_array.shape[1]
+        # self.ms_array = np.full(
+        #     [self.pc_height, self.pc_width, self.ms_bands], NaN)
+
+        # for i in range(self.pc_height):
+        #     for j in range(self.pc_width):
+        #         pt = self.array[i][j]
+
+        #         if not isnan(pt[0]):
+        #             point = np.matrix([[pt[0]], [pt[1]], [pt[2]], [1]])
+
+        #             for n in range(self.ms_bands):
+        #                 point_transformed = np.dot(self.T[n], point)
+
+        #                 u, v = self.ms_camera[n].get_pixel_coordinates(
+        #                     point_transformed)
+        #                 if u is not None and v is not None:
+        #                     self.ms_array[i, j, n] = self.images[n][v, u]
+
+        # self.ndvi_array = np.copy(self.pc_array)
+        # nir_thresh = 0.15
+        ind = np.zeros(12)
+        counter = 0
+
+        nir_total = 0
+        nir_counter = 0
+
+        for i in range(self.lower_bbox[0], self.lower_bbox[2]):
+            for j in range(self.lower_bbox[1], self.lower_bbox[3]):
+                nir_total += self.ms_array[i, j, 3]
+                nir_counter += 1
+
+        nir_thresh = nir_total/nir_counter
+
+        for i in range(self.lower_bbox[0], self.lower_bbox[2]):
+            for j in range(self.lower_bbox[1], self.lower_bbox[3]):
+                blue = self.ms_array[i, j, 0]
+                green = self.ms_array[i, j, 1]
+                red = self.ms_array[i, j, 2]
+                nir = self.ms_array[i, j, 3]
+                re = self.ms_array[i, j, 4]
+
+                if not isnan(nir) and not isnan(red) and not isnan(green) and not isnan(re) and not isnan(blue) and nir > nir_thresh:
+                    counter += 1
+                    ind[0] += (nir-red)/(nir+red)
+                    ind[1] += nir/red
+                    ind[2] += (nir-green)/(nir+green)
+                    ind[3] += 0.16*(nir-red)/(nir+red+0.16)
+                    ind[4] += (2*nir + 1 -
+                               sqrt(pow((2*nir+1), 2) - 8*(nir-red))) / 2
+                    ind[5] += 1.5*(1.2*(nir-green)-2.5*(red-green)) / \
+                        sqrt(pow((3*nir+1), 2)-(6*nir-5*sqrt(red))-0.5)
                     ind[6] += 1.16*(nir-red)/(nir+red+0.16)
                     ind[7] += nir - re/nir + re
                     ind[8] += 2.5*(nir-red) / (nir+6*red-7.5*blue+1)
@@ -306,34 +356,66 @@ class DataCollector:
         avg_indices = ind / counter
         print(avg_indices)
 
-        msg = ros_numpy.point_cloud2.array_to_pointcloud2(self.ndvi_array, frame_id='base')
+        nir_total = 0
+        nir_counter = 0
+
+        for i in range(self.upper_bbox[0], self.upper_bbox[2]):
+            for j in range(self.upper_bbox[1], self.upper_bbox[3]):
+                nir_total += self.ms_array[i, j, 3]
+                nir_counter += 1
+
+        nir_thresh = nir_total/nir_counter
+        for i in range(self.upper_bbox[0], self.upper_bbox[2]):
+            for j in range(self.upper_bbox[1], self.upper_bbox[3]):
+                blue = self.ms_array[i, j, 0]
+                green = self.ms_array[i, j, 1]
+                red = self.ms_array[i, j, 2]
+                nir = self.ms_array[i, j, 3]
+                re = self.ms_array[i, j, 4]
+
+                if not isnan(nir) and not isnan(red) and not isnan(green) and not isnan(re) and not isnan(blue) and nir > nir_thresh:
+                    counter += 1
+                    ind[0] += (nir-red)/(nir+red)
+                    ind[1] += nir/red
+                    ind[2] += (nir-green)/(nir+green)
+                    ind[3] += 0.16*(nir-red)/(nir+red+0.16)
+                    ind[4] += (2*nir + 1 -
+                               sqrt(pow((2*nir+1), 2) - 8*(nir-red))) / 2
+                    ind[5] += 1.5*(1.2*(nir-green)-2.5*(red-green)) / \
+                        sqrt(pow((3*nir+1), 2)-(6*nir-5*sqrt(red))-0.5)
+                    ind[6] += 1.16*(nir-red)/(nir+red+0.16)
+                    ind[7] += nir - re/nir + re
+                    ind[8] += 2.5*(nir-red) / (nir+6*red-7.5*blue+1)
+                    ind[9] += 0.5*(120*(nir-green)-200*(red-green))
+                    ind[10] += (nir-red) / sqrt(nir+red)
+                    ind[11] += nir-red
+
+                    self.ndvi_array[i, j][3] = (nir-red)/(nir+red)
+                else:
+                    self.ndvi_array[i, j][3] = NaN
+
+        avg_indices = ind / counter
+        print(avg_indices)
+
+        msg = ros_numpy.point_cloud2.array_to_pointcloud2(
+            self.ndvi_array, frame_id='base')
 
         while not rospy.is_shutdown():
             pc_pub.publish(msg)
             rospy.sleep(5)
             print('PC published!')
 
-    def find_thresh(self):
-        path = '/home/jelena/bags/agrosparc_test/IMG_0003_4.tif' 
-        img = Image(path).undistorted_reflectance()
-
-        cv2.imshow("test", img)
-        cv2.waitKey()
-
-
-
-    # reflectance, not raw!
     def generate_mspc(self):
         self.pc_array = ros_numpy.point_cloud2.pointcloud2_to_array(
-            self.msg)  # shape (480,640)
+            self.pc_msg)  # shape (480,640)
         self.array = np.copy(self.pc_array)
         self.pc_height = self.pc_array.shape[0]
         self.pc_width = self.pc_array.shape[1]
         self.ms_array = np.full(
             [self.pc_height, self.pc_width, self.ms_bands], NaN)
 
-        for i in range(self.pc_height):
-            for j in range(self.pc_width):
+        for i in range(self.lower_bbox[0], self.lower_bbox[2]):
+            for j in range(self.lower_bbox[1], self.lower_bbox[3]):
                 pt = self.array[i][j]
 
                 if not isnan(pt[0]):
@@ -347,42 +429,79 @@ class DataCollector:
                         if u is not None and v is not None:
                             self.ms_array[i, j, n] = self.images[n][v, u]
 
+        for i in range(self.upper_bbox[0], self.upper_bbox[2]):
+            for j in range(self.upper_bbox[1], self.upper_bbox[3]):
+                pt = self.array[i][j]
+
+                if not isnan(pt[0]):
+                    point = np.matrix([[pt[0]], [pt[1]], [pt[2]], [1]])
+
+                    for n in range(self.ms_bands):
+                        point_transformed = np.dot(self.T[n], point)
+
+                        u, v = self.ms_camera[n].get_pixel_coordinates(
+                            point_transformed)
+                        if u is not None and v is not None:
+                            self.ms_array[i, j, n] = self.images[n][v, u]
+
+        print("Generated MSD")
+
     def capture(self):
         self.stereo_client.update_configuration({'emitter_enabled': 0})
         rospy.sleep(2)
-        self.r = requests.get('http://192.168.10.254/capture', {'block': True, })
+        self.r = requests.get(
+            'http://192.168.10.254/capture', {'block': True, })
         self.stereo_client.update_configuration({'emitter_enabled': 1})
         print("Capture complete")
+
+    def find_thresh(self):
+        path = '/home/jelena/catkin_ws_old/src/micasense_camera/agrosparc_nir_test/IMG_0302_4.tif'
+        #img = Image(path).undistorted_reflectance()
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
+        nir = 0
+        nir_cnt = 0
+
+        for i in range(0, 430):
+            for j in range(510, 670):
+                # for i in range(480,940):
+                #     for j in range(510,670):
+                nir += img[i, j]
+                nir_cnt += 1
+
+        avg = nir/nir_cnt
+
+        for i in range(0, 430):
+            for j in range(510, 670):
+                # for i in range(480,940):
+                #     for j in range(510,670):
+                nir = img[i, j]
+                if nir < 1.5*avg:
+                    img[i, j] = 0
+
+        cv2.imshow("test", img)
+        cv2.waitKey()
+
+    def check_bboxes(self):
+        path = '/home/jelena/catkin_ws_old/src/micasense_camera/agrosparc_nir_test/test/4.jpg'
+
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        img_crop = img[self.lower_bbox[0]:self.lower_bbox[2],
+                       self.lower_bbox[1]:self.lower_bbox[3]]
+        cv2.imshow("test", img_crop)
+        cv2.waitKey()
+
+        img_crop = img[self.upper_bbox[0]:self.upper_bbox[2],
+                       self.upper_bbox[1]:self.upper_bbox[3]]
+        cv2.imshow("test", img_crop)
+        cv2.waitKey()
 
 
 def main():
     rospy.init_node('data_collector', anonymous=True)
-    
-    # path = '/home/jelena/bags/agrosparc_test/IMG_0003_4.tif' 
-
-    # img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    # for y in range(960):
-    #     for x in range(1280):
-    #         nir = img[y,x]
-    #         if nir < 30000: # manje
-    #             img[y,x] = 0
-
-    # img = Image(path).undistorted_reflectance()
-
-    # for y in range(960):
-    #     for x in range(1280):
-    #         nir = img[y,x]
-    #         if nir < 0.15:
-    #             img[y,x] = 0
-
-
-    # cv2.imshow("test", img)
-    # cv2.imwrite('/home/jelena/bags/agrosparc_test/mask.tif', img)
-    # cv2.waitKey()
-
-    
     collector = DataCollector()
     collector.collect_data()
+
 
 if __name__ == "__main__":
     main()
